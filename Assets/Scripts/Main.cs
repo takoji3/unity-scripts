8e3d93da763973f609ff6396288cd6deef59fbb3
takoji3 <takoji3G@gmail.com>
2016-04-20 18:03:38 +0900

    add number sprite script

---
+++ Assets/Scripts/Main.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Main : MonoBehaviour
+{
+	public NumberSprites Numbers;
+	
+	// Use this for initialization
+	void Start ()
+	{
+		StartCoroutine(Increase());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	IEnumerator Increase()
+	{
+		Numbers.Reset(0);
+		while (true)
+		{
+			this.Numbers.Value += Random.Range(10, 20);
+			yield return new WaitForSeconds(2);
+		}
+	}
+}
---
+++ Assets/Scripts/NumberSprites.cs
@@ -0,0 +1,82 @@
+﻿using UnityEngine;
+
+public class NumberSprites : MonoBehaviour
+{
+    [SerializeField]
+    SpriteRenderer[] Renderers;
+
+    [SerializeField]
+    OrderType Order;
+    
+    [SerializeField]
+    Sprite[] SpriteImages = new Sprite[10];
+
+    enum OrderType
+    {
+        Increase,
+        Decrease
+    }
+
+    public int Value { get; set; }
+
+    int displayValue = 0;
+
+    System.Action update;
+
+    System.Action Increase
+    {
+        get
+        {
+            return () =>
+            {
+                if (this.Value > this.displayValue) this.UpdateDisplayValue(++this.displayValue);
+            };
+        }
+    }
+
+    System.Action Decrease
+    {
+        get
+        {
+            return () =>
+            {
+                if (this.Value < this.displayValue) this.UpdateDisplayValue(--this.displayValue);
+            };
+        }
+    }
+
+    public void Reset(int value)
+    {
+        this.displayValue = value;
+        this.Value = value;
+        this.UpdateDisplayValue(value);
+    }
+
+    void Awake()
+    {
+        this.update = this.Order == OrderType.Increase ? this.Increase : this.Decrease;
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+        this.update();
+    }
+
+    void UpdateDisplayValue(int value)
+    {
+        int index = 0;
+        int last = this.Renderers.Length - 1;
+
+        while (index <= last)
+        {
+            int num = value / (int)Mathf.Pow(10, last - index) % 10;
+            this.Renderers[index++].sprite = this.SpriteImages[num];
+        }
+    }
+    
+    Sprite SpriteImage(int num)
+    {
+        return this.SpriteImages[num];
+    }
+}
